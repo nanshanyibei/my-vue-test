87fd830cb751bfc1dc9a9f9eaf7b1a496a95b9ed
nanshanyibei <1641685360@qq.com>
2020-06-02 17:26:33 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,58 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="header">
+      <div class="header-border">
+        <div class="login-button">Register</div>
+        <div class="login-button">Login</div>
+        <div class="login-button">{{username}}</div>
+        <div class="login-button">Logout</div>
+      </div>
+    </div>
     <router-view/>
+    <div class="footer">
+      belong to T12534
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return {
+      username:'TEST'
+    }
+  }
 }
 </script>
 
 <style>
-#app {
+#app{
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  font-size: 16px;
+}
+.header,.footer{
+  width: 100%;
+  background: #2c3e50;
+}
+.header-border{
+  height: 50px;
+  line-height: 50px;
+  color: #ffffff;
+  display: flex;
+  width: 880px;
+  margin: 0 auto;
+}
+.login-button{
+  width: 100px;
+}
+.footer{
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  color: #ffffff;
 }
 </style>
