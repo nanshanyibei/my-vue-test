7d17c68109ff5eb4ccdaa1a8d4749e80734b41fd
nanshanyibei <1641685360@qq.com>
2020-08-19 23:43:38 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,51 @@
 <template>
   <div class="content">
-    <div class="top-container">
-      <div class="top-content">
-        <span>{{onFirstWord}}</span>
-        <span @click="fristWordClick(0)">wine only</span>
-        <span @click="fristWordClick(1)">rose only</span>
-        <span @click="fristWordClick(2)">hamper only</span>
-        <span @click="fristWordClick(3)">fruit only</span>
-      </div>
-    </div>
-    <div class="center-content">
-      <div class="center-content-element">
-        <div class="one-picture">一张图片</div>
-        <div class="one-word">一些文字</div>
-      </div>
-      <div class="center-content-element">
-        <div class="one-picture">另一张图片</div>
-        <div class="one-word">另一些文字</div>
-      </div>
-      <div class="center-content-element">1300 767 376</div>
-    </div>
-    <div class="router-container">
-      <div class="router-container-body">
-        <div class="router-container-body-word">SPECIALS</div>
-        <div class="router-container-body-word">BIRTHDAY</div>
-        <div class="router-container-body-word">ROMANCE</div>
-        <div class="router-container-body-word">NEW BABY</div>
-        <div class="router-container-body-word">SYMPATHY</div>
-        <div class="router-container-body-word">GET WALL</div>
-        <div class="router-container-body-word">ALL FLOWSER</div>
+    <div class="top-navigation-bar">
+      <div class="top-navigation-bar-before">
+        <el-menu :default-active="activeIndex" class="el-menu-demo position-in-center" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="1">Home</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">GIFTS FOR HER</template>
+            <el-menu-item index="2-1">Fashion</el-menu-item>
+            <el-menu-item index="2-2">Beauty</el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">GIFTS FOR HIM</template>
+            <el-menu-item index="3-1">Fashion</el-menu-item>
+            <el-menu-item index="3-2">Beauty</el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">GIFTS FOR US</template>
+            <el-menu-item index="4-1">Matching Sets</el-menu-item>
+            <el-menu-item index="4-2">Gourmet Hamners</el-menu-item>
+            <el-menu-item index="4-3">Hotel Packages</el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">CUSTOMER SERVICE</template>
+            <el-menu-item index="5-1">About Us</el-menu-item>
+            <el-menu-item index="5-2">FA Questions</el-menu-item>
+            <el-menu-item index="5-3">Blog</el-menu-item>
+            <el-menu-item index="5-4">Security & Refunds</el-menu-item>
+            <el-menu-item index="5-5">Returns & Refunds</el-menu-item>
+            <el-menu-item index="5-6">Join the Newslettery</el-menu-item>
+            <el-menu-item index="5-7">Terms & Conditions</el-menu-item>
+            <el-menu-item index="5-8">Deliver & Payment</el-menu-item>
+            <el-menu-item index="5-9">Contact Us</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="6">Account</el-menu-item>
+          <el-menu-item index="7">Login/Register</el-menu-item>
+        </el-menu>
       </div>
     </div>
     <router-view/>
-    <div class="zhanwei"></div>
-    <div class="footer">
-      <div class="footer-body">
-        <div>Information</div>
-        <div>The Fine Print</div>
-        <div>Style</div>
-        <div>By Price</div>
+    <div class="footer-navigation">
+      <div class="footer-navigation-top">E-BOOKS/LINKS</div>
+      <div class="footer-navigation-center">Follow us on Social Media:
+        <span>Facebook</span> /
+        <span>Instagram</span> /
+        <span>Twitter</span> /
+        <span>Linkedln</span> /
+        <span>YouTube</span>
       </div>
     </div>
   </div>
@@ -54,109 +61,42 @@ export default {
   },
   methods:{
     fristWordClick(num){
-      switch(num){
-        case 0:
-          this.onFirstWord = 'wine only'
-          break
-        case 1:
-          this.onFirstWord = 'rose only'
-          break
-        case 2:
-          this.onFirstWord = 'hamper only'
-          break
-        case 3:
-          this.onFirstWord = 'fruit only'
-          break
-      }
     }
   }
 }
 </script>
 
 <style scoped>
-.top-container{
+.top-navigation-bar-before::before{
+  display: block;
+  content: 'MON CHERI GIFTS';
+  border-bottom: 1px solid #000;
   width: 100%;
-  height: 102px;
-  background: linear-gradient(#fff,#e1e1e1);
-  border-bottom: 1px solid #d2d2d2;
-  display: flex;
-  align-items: flex-end;
-}
-.top-content{
-  width: 1000px;
-  margin: 0 auto;
-}
-.top-content span:first-child{
-  transform: scale(1.6);
-}
-.top-content span:nth-child(2){
-  margin-left: 44px;
-}
-.top-content span{
-  display: inline-block;
-  width: 142px;
-  height: 62px;
-  background: goldenrod;
-  border: 1px solid #000;
-  line-height: 62px;
-  font-size: 18px;
+  height: 40px;
+  line-height: 40px;
   text-align: center;
-  cursor: pointer;
+  font-size: 16px;
 }
-.center-content{
-  display: flex;
-  width: 1200px;
-  margin-left: 360px;
+.top-navigation-bar{
+  border-bottom: 1px solid #000;
 }
-.center-content-element{
-  width: 400px;
-  border: 1px solid #ff6666;
-  display: flex
-}
-.one-picture{
-  width: 60px;
-  height: 60px;
-  background: #ffcccc;
-}
-.router-container{
-  width: 100%;
-  background: #333;
-  height: 44px;
-  color: #fff;
-}
-.router-container-body{
-  display: flex;
-  width: 880px;
+.position-in-center{
+  width: fit-content;
   margin: 0 auto;
-  justify-content: space-evenly
+  border: none;
 }
-.router-container-body-word{
-  line-height: 44px;
-  text-align: center;
-  cursor: pointer;
-  font-weight: normal;
-  font-family: BrandonL,Helvetica,sans-serif;
-  text-transform: uppercase;
-  font-size: 15px;
+.footer-navigation{
+  border-top: 1px solid #000;
+  border-bottom: 1px solid #000;
+  font-size: 16px;
 }
-.router-container-body-word:hover{
-  color: #3d3d3d;
+.footer-navigation-center{
+  text-decoration: underline;
+  width: fit-content;
+  margin: 0 auto 15px;
 }
-.zhanwei{
-  width: 100%;
-  height: 200px;
-}
-.footer{
-  width: 100%;
-  height: 100px;
-  background: #353535;
-}
-.footer-body{
-  width: 880px;
-  color: #fff;
-  font-size: 24px;
-  display: flex;
-  margin: 0 auto;
-  justify-content: space-evenly;
+.footer-navigation-top{
+  width: fit-content;
+  margin: 5px auto 15px;
 }
 </style>
---
+++ src/components/Register.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="register-content">
+    <div class="moncherigift">Moncherigift</div>
+    <div class="register-content-container">
+      <el-input v-model="accountName" class="register-element" placeholder="account name" />
+      <el-input v-model="password" class="register-element" placeholder="Password" />
+      <el-input v-model="firstName" class="register-element" placeholder="First Name" />
+      <el-input v-model="lastName" class="register-element" placeholder="Las tName" />
+      <el-input v-model="emailAddress" class="register-element" placeholder="Email" />
+      <el-input v-model="mobile" class="register-element" placeholder="Mobile" />
+      <div class="bottom-words">
+        By creating an account, you agree that you have read and accepted our Conditions of
+        Use and Privacy Notice.
+      </div>
+    </div>
+    <el-button class="create-account">Create account</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      accountName: '',
+      password: '',
+      firstName: '',
+      lastName: '',
+      emailAddress: '',
+      mobile: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.moncherigift{
+  width: 500px;
+  margin: 150px auto 0;
+  border: 1px solid #000;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+}
+.register-content-container{
+  width: 400px;
+  margin: 0 auto;
+  border: 1px solid #000;
+  border-top: none;
+  padding-top: 20px;
+}
+.register-element{
+  width: 300px;
+  margin: 0 auto 20px;
+  display: block;
+}
+.bottom-words{
+  width: 300px;
+  margin: 0 auto 20px;
+}
+.create-account{
+  border: 1px solid #000;
+  margin: 40px auto 150px;
+  display: block;
+  width: fit-content;
+}
+</style>
